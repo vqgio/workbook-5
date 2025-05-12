94359d6916f9d34857892a4b421262579b726db0
Gio <giovannizavala82@gmail.com>
2025-05-12 15:30:24 -0500

    working on all classes, they now have super to call vehicle class

--- Vehicles1/src/main/java/com/pluralsight/Car.java
+++ Vehicles1/src/main/java/com/pluralsight/Car.java
@@ -2,9 +2,23 @@ package com.pluralsight;
 
 public class Car extends Vehicle {
     String trim;
+    int gasType;
 
-    public Car(String color, int numberOfPassengers, int cargoCapacity, int fuelCapacity, String trim) {
+    public Car(String color, int numberOfPassengers, int cargoCapacity, int fuelCapacity, String trim, int gasType) {
         super(color, numberOfPassengers, cargoCapacity, fuelCapacity);
         this.trim = trim;
+        this.gasType = gasType;
+    }
+
+    public int getGasType() {
+        return gasType;
+    }
+
+    public void setGasType(int gasType) {
+        if (gasType < 87) {
+            System.out.println("Invalid gas type");
+        } else {
+            this.gasType = gasType;
+        }
     }
 }
