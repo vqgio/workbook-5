12ce1c9210c60bd6990b34e87f1c79af5e3db43b
Gio <giovannizavala82@gmail.com>
2025-05-13 15:35:50 -0500

    exercise 1

--- AssetManager/src/main/java/com/pluralsight/Main.java
+++ AssetManager/src/main/java/com/pluralsight/Main.java
@@ -6,6 +6,14 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Asset> assets = new ArrayList<>();
         assets.add(new House("My house", "04-16-2013", 115000, "10001 South 1st Street", 3, 2400, 9000));
+        assets.add(new House("Vacation home", "07-22-2022", 355000, "3844 John Pork Drive", 4, 5000, 11000));
 
+        assets.add(new Vehicle("Gio's car", "03-22-2023", 6000, "Infinti G35", 2010, 151000));
+        assets.add(new Vehicle("Alice's Car", "11-19-2024", 11000, "Cadillac XT5", 2017, 79000));
+
+        for (Asset asset : assets) {
+            System.out.println("Description: " + asset.getDescription());
+            System.out.println("Date acquired: " + asset.getDateAquired());
+        }
     }
 }
